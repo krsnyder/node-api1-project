84abe12b2b1335865e92b01d15eec2f01a7b9dcc
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-05 15:13:29 -0400

    Built both GET requests and successfully received data back from server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,47 @@
-// BUILD YOUR SERVER HERE
+const express = require("express")
+const Users = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message})
+    })
+})
+
+server.get('/api/users/:id', (req,res) => {
+  const { id } = req.params;
+  console.log(id)
+  Users.findById(id)
+    .then(user => {
+      console.log(user)
+      res.status(200).json(user)
+    })
+    .catch(err => {
+    res.status(422).json({message: err.message})
+  })
+})
+
+// server.post('/api/users', (req, res) => {
+  
+// })
+
+// server.put('/api/users:id', (req, res) => {
+  
+// })
+
+// server.delete('/api/users/:id', (req, res) => {
+  
+// })
+
+server.use("*", (req, res) => {
+  res.status(404).json({message: "Let's get this party rockin"})
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./api/server');
 
-const port = 5000;
-
 // START YOUR SERVER HERE
+
+server.listen(8000, () => {
+  console.log("Run it")
+})
